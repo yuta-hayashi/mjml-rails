d758fb901ed9b17ac6021979abc68c218141e6ea
Ryan Ahearn <ryan.c.ahearn@gmail.com>
2018-06-28 13:19:51 -0400

    Update for mjml-core 4.1.0

--- test/template_test.rb
+++ test/template_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
 class MjmlTest < ActiveSupport::TestCase
-  
+
   test 'with Mjmltemplate processor' do
-    assert_not_equal "<mj-body></mj-body>", Mjml::Mjmltemplate.to_html("<mjml><mj-body><mj-container><mj-section><mj-column></mj-column></mj-section></mj-container></mj-body></mjml>").strip
+    assert_not_equal "<mj-body></mj-body>", Mjml::Mjmltemplate.to_html("<mjml><mj-body><mj-section><mj-column></mj-column></mj-section></mj-body></mjml>").strip
   end
 
 end
